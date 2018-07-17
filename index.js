7198c1feb357f55661ed24e5b029612914f8155e
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-17 21:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ function bumpCounter() {
  const {addBump, getBumps} = bumpCounter()
 
  function createAnimal(animal) {
+   
     function deadlyDevice(device) {
       var obj = {}
       obj.animalType = animal
       obj.deadlyDevice = device
       return obj;
     }
-  return deadlyDevice
+    
+  return deadlyDevice;
 }
 
 sharkCreator = createAnimal('Shark')
